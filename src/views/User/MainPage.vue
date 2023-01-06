34992ffeaf1a2ac4073961bb3983c67361eb08e3
sadirov4647 <sadirovizzatillo3@gmail.com>
2023-01-06 19:58:41 +0500

    v1

--- src/views/User/MainPage.vue
+++ src/views/User/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="main-products">
-        <SingleProduct :product="product" v-for="(product, id) in products" :key="id" />
+      <SingleProduct :product="product" v-for="(product, id) in products" :key="id" />
     </div>
   </div>
 </template>
@@ -22,7 +22,24 @@ const products = computed(() => {
 
 <style>
 .main-products{
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+@media screen and (max-width:950px) {
+  .main-products{
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
+@media screen and (max-width:720px) {
+  .main-products{
+    grid-template-columns: 1fr 1fr;
+  }
+}
+@media screen and (max-width:500px) {
+  .main-products{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 }
 </style>
