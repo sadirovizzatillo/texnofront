c530e678de269f98f4ed9695caa802c177c34337
sadirov4647 <sadirovizzatillo3@gmail.com>
2023-01-08 14:23:38 +0500

    v1

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://texnomartback.onrender.com/api",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
